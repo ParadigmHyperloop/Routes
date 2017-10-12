8468ddb0f6f6a7d54f479b8a9ed2955fc8719285
BlueSpud <logan2299@yahoo.com>
2017-10-11 23:15:14 -0400

    Sample OpenCL kernel

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 
 namespace compute = boost::compute;
 
+int test() { return 1; }
+
 int main()
 {
     // get the default compute device
@@ -15,28 +17,50 @@ int main()
 
     // generate random numbers on the host
     std::vector<float> host_vector(1000000);
-    std::generate(host_vector.begin(), host_vector.end(), rand);
+    std::generate(host_vector.begin(), host_vector.end(), test);
 
     // create vector on the device
     compute::vector<float> device_vector(1000000, ctx);
+    compute::vector<float> result_vector(1000000, ctx);
 
     // copy data to the device
     compute::copy(
         host_vector.begin(), host_vector.end(), device_vector.begin(), queue
     );
 
-    // sort data on the device
-    compute::sort(
-        device_vector.begin(), device_vector.end(), queue
+    std::string source = BOOST_COMPUTE_STRINGIZE_SOURCE(
+
+            kernel void add(global float* values, global float* results)
+            {
+                size_t index = get_global_id(0);
+                for (int i = 0; i < 2; i++) {
+
+                    results[index] += values[index] + values[index + 1];
+
+                }
+            }
+
     );
+
+    boost::shared_ptr<boost::compute::program_cache> global_cache = boost::compute::program_cache::get_global_cache(ctx);
+    std::string options;
+    std::string key = "__iwocl16_saxpy";
+
+    boost::compute::program program = global_cache->get_or_build(key, options, source, ctx);
+
+    boost::compute::kernel kernel = program.create_kernel("add"); kernel.set_args(device_vector.get_buffer(), result_vector.get_buffer());
+    queue.enqueue_1d_range_kernel(kernel, 0, device_vector.size() - 1, 0);
+
 
     // copy data back to the host
+    //host_vector.clear();
+    //host_vector.resize(1000000);
+
     compute::copy(
-        device_vector.begin(), device_vector.end(), host_vector.begin(), queue
+            result_vector.begin(), result_vector.end(), host_vector.begin(), queue
     );
 
-    for (int i = 0; i < host_vector.size(); i++)
-        std::cout << host_vector[i] << std::endl;
+    std::cout << "Size " << host_vector[0] << std::endl;
 
     return 0;
 }
