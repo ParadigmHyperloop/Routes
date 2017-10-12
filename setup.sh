04c4c8a26fa73f0c815fd0b98d90b7a864228321
BlueSpud <logan2299@yahoo.com>
2017-10-11 20:50:50 -0400

    Boost compute

--- setup.sh
+++ setup.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
-git clone https://github.com/OSGeo/gdal.git
-cd gdal/gdal
-./configure
-make
-sudo make install
-cd ../..
-rm gdal
+
+if [ -f /usr/local/lib/libgdal.a ] 
+then
+
+	echo "Found GDAL installed"
+else
+
+	git clone https://github.com/OSGeo/gdal.git
+	cd gdal/gdal
+	./configure
+	sudo make
+	sudo make install
+	cd ../..
+	rm -rf gdal
+
+fi
+
+if [ -f ./include ]
+then
+
+	echo "Include directory already exists"
+
+else
+
+	mkdir include
+
+fi
+
+git clone https://github.com/boostorg/compute.git
+cp -r ./compute/include/boost ./include/boost
+rm -rf compute
