f4c711be85c331cee389a673f227ee88eba6ca24
isaac <walker.i@husky.neu.edu>
2017-10-13 14:02:49 -0400

    Added a Constructor for Pod

--- src/pod/pod.h
+++ src/pod/pod.h
@@ -7,52 +7,50 @@
 
 #include <math.h>
 
-class pod {
+class Pod {
     public:
 
         /**
-         * Void -> Double
-         * Returns the acceleration due to gravity (g) at the Earth's surface in m/s^2
          * @return
+         * Returns the acceleration due to gravity (g) at the Earth's surface in m/s^2
          */
-        static double g()
+        static float g()
         {return 9.81;}
 
         /**
-         * Void -> Double
-         * Returns the maximum slope grade (6%) that the track will have
          * @return
+         * Returns the maximum slope grade (6%) that the track will have
          */
-        double maxSlopeGrade()
+        float maxSlopeGrade()
         {return 0.06;}
 
         /**
-         * Void -> Double
-         * Returns the maximum speed of the pod in  m/s
          * @return
-         */
-        static double maxSpeed()
-        {return 339.0;}
-
-        /**
-         * Void -> Double
-         * Returns the minimum radius of curvature that the track may have in meters
-         * This is based on the maximum speed of the hyperloop pod in m/s
-         * and a max lateral acceleration of 1/2 g
-         * @return
-         */
-        static double minCurveRadius()
-        {return pow (maxSpeed(), 2) / 2*g(); }
-
-        /**
-         * Void -> Double
          * Returns the maximum depth that a hyperloop track can go underground where
          * basic excavation with suffice.
-         * @return
          */
-        static double excavationDepth()
+        static float excavationDepth()
         {return 10.0;}
 
+        /**
+        * Void -> Double
+        * Returns the minimum radius of curvature that the track may have in meters
+        * This is based on the maximum speed of the hyperloop pod in m/s
+        * and a max lateral acceleration of 1/2 g
+        * @return
+        */
+        float minCurveRadius()
+        {return pow (maxSpeed(), 2) / 2*g(); }
+
+        Pod(float max_speed);
+
+    private:
+        /**
+         * @return
+         * Returns the maximum speed of the pod in  m/s
+         */
+        float maxSpeed()
+        {return 339.0;}
 };
 
 
