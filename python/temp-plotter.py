f45452033f90f4212564894b4e024a000e1936cc
isaac <walker.i@husky.neu.edu>
2017-10-16 01:36:46 -0400

    Added a time calculating function

--- python/temp-plotter.py
+++ python/temp-plotter.py
@@ -2,7 +2,29 @@ import matplotlib.pyplot as plt, matplotlib.cm as cm
 import bezier
 import numpy
 
-sol = [[0, 0], [21873.1, 31872.5], [47215.7, 14431.9], [27216.6, 52574.7], [66803.6, 47882.7], [72061.1, 58684.2], [93529.5, 59533.7], [70000, 80000]]
+#speed of pod
+MACH1 = 343 #m/s
+
+# The solution points for the bezier curve
+sol = [[0, 0], [14363.6, 33499.3], [26420.7, 28297], [41581.4, 39071.7], [34848.8, 62412.5], [27620.8, 28238.9], [59134.4, 74852.1], [70000, 80000]]
+
+#define the bezier curve
 curve = bezier.Curve(numpy.array(sol), len(sol) - 1)
-curve.plot(3700)
+
+#calculate the length of the bezier curve
+arcLength = curve.length
+
+#calculate the time to travel the length of the tube
+time = (arcLength / (.95 * MACH1)) / 60 
+
+#define the current plot
+currentPlot = curve.plot(3700)
+
+#create the string to be overlayed on the graph
+timeString = "Time to travel the route at Mach1: " + str(time) + " mins"
+
+#annotate the graph
+currentPlot.annotate(timeString, xy=(7000,0), xytext=(7000,0))
+
+#output the graph to a file
 plt.savefig("plot.png") 
