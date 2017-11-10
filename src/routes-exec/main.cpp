0711364bad72d3d5cab3b0ed86b075085d6e4a00
BlueSpud <logan2299@yahoo.com>
2017-11-10 12:06:38 -0500

    Fixed Windows VTF rebuilding

--- src/routes-exec/main.cpp
+++ src/routes-exec/main.cpp
@@ -2,6 +2,7 @@
 // Created by logan on 11/3/2017.
 //
 
+#include <boost/filesystem.hpp>
 #include <routes.h>
 #include <stdlib.h>
 #include "cmd/cmd.h"
@@ -27,11 +28,38 @@ int main(int argc, const char* argv[]) {
 
             std::cout << "Rebuilding database\n";
 
-            // Run a command to generate the vtf
-            system("gdalbuildvrt ../data/db.vtf ../data/*.img");
-            
-            // Generate the stats for it
-            system("gdalinfo ../data/db.vtf -stats");
+            #ifdef BOOST_WINDOWS
+
+                std::string build_cmd = "cmd /S /C gdalbuildvrt ../data/db.vtf ";
+
+                // Open up the directory in the boost filesystem
+                boost::filesystem::path data_path = boost::filesystem::path("../data/");
+
+                for(auto& entry : boost::make_iterator_range(boost::filesystem::directory_iterator(data_path), {})) {
+
+                    // Make sure that this file is a .img file
+                    std::string path_string = entry.path().string();
+
+                    if (path_string.length() > 3 && !path_string.substr(path_string.length() - 4, 4).compare(".img"))
+                        build_cmd += path_string + " ";
+
+                }
+
+                // Run a command to generate the vtf
+                system(build_cmd.c_str());
+
+                // Generate the stats for it
+                system("cmd /S /C gdalinfo ../data/db.vtf -stats");
+
+            #else
+
+                // Run a command to generate the vtf
+                system("gdalbuildvrt ../data/db.vtf ../data/*.img");
+
+                // Generate the stats for it
+                system("gdalinfo ../data/db.vtf -stats");
+
+            #endif
 
             break;
 
