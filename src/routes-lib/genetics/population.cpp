0241764270639a95d3d700a3c5e85b78b70fa978
BlueSpud <logan2299@yahoo.com>
2017-11-19 16:25:23 -0500

    Sanitized the cost in the cost function

--- src/routes-lib/genetics/population.cpp
+++ src/routes-lib/genetics/population.cpp
@@ -191,7 +191,7 @@ void Population::evaluateCost(const Pod& pod) {
             float denom = length(cross(der_first0, der_second));
             float num = pown(length(der_first0), 3);
 
-            return num / denom;
+            return fabs(num / denom);
 
         }
 
@@ -202,10 +202,13 @@ void Population::evaluateCost(const Pod& pod) {
                            float num_points_1, int points_per_worker) {
 
             const sampler_t sampler = CLK_NORMALIZED_COORDS_TRUE | CLK_ADDRESS_CLAMP_TO_EDGE | CLK_FILTER_NEAREST;
-            const float pylon_cost = 116.0;
-            const float tunnel_cost = 310000.0;
+            const float pylon_cost = 0.000116;
+            const float tunnel_cost = 0.310;
+            const float curve_weight = 300000.0;
+            const float grade_weight = 100.0;
 
-            __local float min_curves [50];
+            __local float curve_sums [50];
+//            __local float min_curves [50];
             __local float max_grades [50];
             __local float track_costs[50];
 
@@ -215,7 +218,8 @@ void Population::evaluateCost(const Pod& pod) {
 
             int path = i * (path_length + 1) + 1;
 
-            float min_curve = 10000000000000000.0;
+//            float min_curve = 10000000000000.0;
+            float curve_sum = 0.0;
 
             float track_cost = 0.0;
             float steepest_grade = 0.0;
@@ -227,41 +231,52 @@ void Population::evaluateCost(const Pod& pod) {
             float4 last_last = individuals[path];
             float4 last_point = individuals[path];
 
+            // If we start at an offset, we need to make sure we "remember" the information that the last worker
+            // calculated. The fastest way to do this is to just re-calculate it
             if (w) {
 
-                last_last =  evaluateBezierCurve(individuals, path, path_length, (float)(start - 2) / num_points_1, binomial_coeffs);
-                last_point = evaluateBezierCurve(individuals, path, path_length, (float)(start - 1) / num_points_1, binomial_coeffs);
+                last_last   = evaluateBezierCurve(individuals, path, path_length, (float)(start - 2) / num_points_1, binomial_coeffs);
+                last_point  = evaluateBezierCurve(individuals, path, path_length, (float)(start - 1) / num_points_1, binomial_coeffs);
 
             }
 
             for (int p = start; p <= end; p++) {
 
+                // Evaluate the bezier curve
                 float4 bezier_point = evaluateBezierCurve(individuals, path, path_length, (float)p / num_points_1, binomial_coeffs);
 
-                // Get pylon height with a sample from the image
+                // Get the elevation of the terrain at this point. We do this with a texture sample
                 float2 nrm_device = (float2)(bezier_point.x / width, bezier_point.y / height);
                 float height = read_imagef(image, sampler, nrm_device).x;
 
-                // Compute spacing, only x and y distance
+                // Compute spacing, only x and y distance, z delta is handled by the grade
                 float spacing = sqrt(pown(bezier_point.x - last_point.x, 2) + pown(bezier_point.y - last_point.y, 2));
 
                 // Get curvature
-                if (p > 1)
-                    min_curve = min(min_curve, curvature(last_last, last_point, bezier_point));
+                if (p > 1) {
 
-                // Compute grade if the points had spacing
+                    float curve = curvature(last_last, last_point, bezier_point);
+                    curve_sum += 1.0 / curve * curve_weight;
+//                    min_curve = min(min_curve, curve);
+
+                }
+
+                // Compute grade and track cost if there was spacing
                 if (spacing) {
 
                     steepest_grade = max(steepest_grade, fabs(bezier_point.z - last_point.z) / spacing);
 
                     // Compute track cost
+                    // First we get the pylon height which is the distance from the point on the track to the actual terrain
                     float pylon_height = bezier_point.z - height;
 
-                    // Above cost
+                    // Cost for the track being above the terrain. This is significantly less than if it was
+                    // underground because no tunneling is needed
                     float above_cost = 0.5 * (fabs(pylon_height) + pylon_height);
                     above_cost = pown(above_cost, 2) * pylon_cost;
 
-                    // Below cost
+                    // Cost for the track being below the ground.
+                    // For a delta of <= excavation_depth we don't count as tunneling because excavation will suffice
                     float below_cost = (-fabs(pylon_height + excavation_depth) + pylon_height + excavation_depth);
                     float below_cost_den = 2.0 * pylon_height + 2.0 * (excavation_depth);
 
@@ -270,39 +285,46 @@ void Population::evaluateCost(const Pod& pod) {
 
             }
 
+                // Keep track of the stuff that was computed so that we can calculate the delta at the next point
                 last_last = last_point;
                 last_point = bezier_point;
 
             }
 
             // Write to the buffer
-            min_curves [w] = min_curve;
-            max_grades [w] = min_curve_allowed;
+            curve_sums [w] = curve_sum;
+//            min_curves [w] = min_curve;
+            max_grades [w] = steepest_grade;
             track_costs[w] = track_cost;
 
             barrier(CLK_LOCAL_MEM_FENCE);
 
+            // This is only done on thread 0 so we don't do a million memory writes/reads
             if (!w) {
 
-                // Figure out real min curve and max grade
+                // Figure out the final cost for everything. This would be equivalent to using one thread
                 for (int m = 1; m < 50; m++) {
 
-                     min_curve = min(min_curve, min_curves[m]);
+                     curve_sum += curve_sums[m];
+//                     min_curve = min(min_curve, min_curves[m]);
                      steepest_grade = max(steepest_grade, max_grades[m]);
                      track_cost += track_costs[m];
 
                 }
 
-                // Calculate grade cost
-                float grade_cost = 100.0 * (steepest_grade - max_grade_allowed + fabs(max_grade_allowed - steepest_grade)) + 1.0;
-                float curve_cost = 0.5 * (min_curve_allowed - min_curve + fabs(min_curve_allowed - min_curve)) + 1.0;
+                // Now that we have all of the information about the route, we can calculate the cost.
+                // Calculate grade cost, only apply a penalty if it is above 6%
+                float grade_cost = grade_weight * (steepest_grade - max_grade_allowed + fabs(max_grade_allowed - steepest_grade)) + 1.0;
+
+                // Calculate the curvature cost
+                // Right now we are simply using the sum of curvature (not radius of curvature)
+                float curve_cost = curve_sum;
 
                 // Get total cost
-                float total_cost = grade_cost * track_cost * curve_cost;
+                float total_cost = grade_cost + track_cost + curve_cost;
 
                 // Set the individual's header to contain its cost
                 individuals[path - 1].x = total_cost;
-                individuals[path - 1].y = min_curve;
 
             }
     });
@@ -445,3 +467,4 @@ float Population::generateRandomFloat(float low, float high) {
     return (random * (high - low)) + low;
     
 }
+          
