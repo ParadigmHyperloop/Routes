e36fe4d84c3a4b16b858485a0147b4352cbc143f
Logan <logan2299@yahoo.com>
2017-12-01 14:50:02 -0500

    Fixed another thing (im really bad at git)

--- src/routes-lib/genetics/population.cpp
+++ src/routes-lib/genetics/population.cpp
@@ -467,16 +467,11 @@ void Population::updateParams() {
     // Update the step size path
    updatePSigma();
 
-<<<<<<< HEAD
    // Update the covariance matrix path
    updatePCovar();
 
    // Update the covariance matrix
    updateCovar();
-=======
-   // Update the covariance matrix
-   updatePCovar();
->>>>>>> CMA
 
    // Update the step size
    updateSigma();
