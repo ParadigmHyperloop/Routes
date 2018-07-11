4060d5b4760634cbd873c6e6c040ba4c30fb3852
walkeri <walker.i@husky.neu.edu>
2018-07-11 12:39:56 -0400

    Normalize all the line endings again

--- src/routes-lib/pod/pod.cpp
+++ src/routes-lib/pod/pod.cpp
@@ -1,1 +1,179 @@
-//// Created by isaac on 10/13/17.//#include "pod.h"Pod::Pod(float max_speed) {    // Save the max speed of the pod    _max_speed = max_speed;}float Pod::minCurveRadius() const { return pow (_max_speed, 2.0) / g; }float Pod::calcCentripetalAccel(glm::vec3 p0, glm::vec3 p1, glm::vec3 p2) const {    float accel = 0.0;    accel = pow(_max_speed, 2) / Bezier::radiusOfCurvature(p0,p1,p2);    return accel;}//TODO: Account for going vertical movementfloat Pod::timeForCurve(const std::vector<glm::vec3>& points) const {    // The length of the track. This is calculated by adding the straight line distance between each point.    float length = Bezier::bezierLength(points);    //map from index to length of track at that index    std::unordered_map<int, float> lengthMap = Bezier::bezierLengthMap(points);    /*     * This will also initialize the accelerations along the track. At the beginning and end of the track, the acceleration is     * determined by the accel needed to get the pod up to speed. Between these points, the acceleration is the     * centripetal acceleration at the point. In the future, centripetal accel should be accounted for in the beginning     * and end of the route.     */    std::vector<float> accels;    float vfSquared = std::pow(DEFAULT_POD_MAX_SPEED, 2);    float v0Squared = 0.0;    //initialize velocity vector, the first value should be 0    std::vector<float> vels;    vels.push_back(0);    //used for getting the last index that the pod is ramping up at    float newVel1 = 0.0;    //Calculate velocities for ramping up, and fill the rest of the route with DEFAULT_POD_MAX_SPEED    for (int i = 1; i < points.size() - 1; i++) {        if (vels[i - 1] < DEFAULT_POD_MAX_SPEED) {            newVel1 = glm::sqrt(glm::pow(vels[i - 1], 2) + (2 * BEGIN_ACCEL * (lengthMap[i] - lengthMap[i - 1])));            vels.push_back(newVel1);        } else {            vels.push_back(DEFAULT_POD_MAX_SPEED);        }    }    //Get the index where you stop ramping up    int count = 0;    for (int i = 0; i < points.size(); i++) {        if (vels[i] < DEFAULT_POD_MAX_SPEED) {            count += 1;        } else {            count += 0;        }    }    int accel_upto_index = count - 1;    //used for getting the first index that the pod is winding down at    float newVel2 = 0.0;    //set all values after ramping up to 0 for velocity so that we can work backwards in next loop    for (int i = accel_upto_index + 1; i < points.size(); i++) {        vels[i] = 0.0;    }    //calculate where to start de-accelerating by working from the end of the route. Set all values in between    // to max accel    for (int i = points.size() - 2; i > accel_upto_index; i--) {        if (vels[i + 1] < DEFAULT_POD_MAX_SPEED) {            newVel2 = glm::sqrt(glm::pow(vels[i+1], 2) + (2 * END_ACCEL * (lengthMap[i+1] - lengthMap[i])));            vels[i] = newVel2;        } else {            vels[i] = DEFAULT_POD_MAX_SPEED;        }    }    //get the index where you start winding down    int count2 = 0;    for (int i = points.size() - 1; i > accel_upto_index; i--) {        if (vels[i] < DEFAULT_POD_MAX_SPEED) {            count2 += 1;        } else {            count += 0;        }    }    count2 = points.size() - count2;    int start_de_accel = count2;    //convert the indices for winding up and ramping down to a point in meters on the curve    float begin_accel_distance = lengthMap[accel_upto_index];    float end_accel_distance = lengthMap[start_de_accel];    //fill in the accelerations of the curve, use the centrip accel when in middle of curve    for (int i = 0; i < points.size() - 1; i++) {        if (lengthMap[i] <= begin_accel_distance) {            accels.push_back(BEGIN_ACCEL);        } else if (lengthMap[i] >= end_accel_distance) {            accels.push_back(END_ACCEL * -1);        } else if (lengthMap[i] > begin_accel_distance && lengthMap[i] < end_accel_distance) {            accels.push_back(calcCentripetalAccel(points[i - 1], points[i], points[i + 1]));        }    }    // Now we need to find all the points where the acceleration is larger than "g". For now we will ignore the    // beginning and the end of the route.    // this is a vector of the indices in the points and accels array where the acceleration is larger than g.    std::vector<int> accels_too_high;    // ignore first and last points    for (int i = accel_upto_index + 1; i < start_de_accel; i++) {        float centrip = calcCentripetalAccel(points[i - 1], points[i], points[i + 1]);        if (centrip > g) {            accels_too_high.push_back(i);        }    }    // Now we must go to all these points, using the velocity, and bringing it down to the velocity that it must be    // in order to make the centripetal acceleration at a good level.    for (int i = accel_upto_index + 1; i < accels_too_high.size(); i++) {        float radiusAtPoint = Bezier::radiusOfCurvature(points[accels_too_high[i] - 1],                                                        points[accels_too_high[i]],                                                        points[accels_too_high[i] + 1]);        float targetVel = glm::sqrt(g * radiusAtPoint);        float distanceToAccel = (_max_speed - targetVel) / (2 * DE_ACCEL);        float distanceFromStart = lengthMap[accels_too_high[i]];        float distanceToStartAccel = distanceFromStart - distanceToAccel;        float startIndex = 0.0;        //get index where slow down needs to start        for (int a = 0; a < accels_too_high[i]; a++) {            startIndex = lengthMap[a];            if (startIndex > distanceToStartAccel) {                a = accels_too_high[i];            }        }        // Correct the velocities. After this the accelerations and velocities are done.        for (int j = startIndex; j < accels_too_high[i]; j++) {            if (lengthMap[j] > (distanceFromStart - distanceToAccel)) {                vels[j] = glm::sqrt(glm::pow(vels[j-1], 2) + (2 * -1 * DE_ACCEL * (lengthMap[i] - lengthMap[i-1])));            }        }    }    //We now calculate the time for the pod to traverse the route    float time = 0.0;    float dist = 0.0;    //Calculate times.    for (int i = 1; i < vels.size() - 1 ; i++) {        dist = lengthMap[i+1] - lengthMap[i];        time += (dist / vels[i]);    }    return time;}
+//
+// Created by isaac on 10/13/17.
+//
+
+
+#include "pod.h"
+
+Pod::Pod(float max_speed) {
+
+    // Save the max speed of the pod
+    _max_speed = max_speed;
+
+}
+
+float Pod::minCurveRadius() const { return pow (_max_speed, 2.0) / g; }
+
+float Pod::calcCentripetalAccel(glm::vec3 p0, glm::vec3 p1, glm::vec3 p2) const {
+
+    float accel = 0.0;
+
+    accel = pow(_max_speed, 2) / Bezier::radiusOfCurvature(p0,p1,p2);
+
+    return accel;
+}
+
+//TODO: Account for going vertical movement
+float Pod::timeForCurve(const std::vector<glm::vec3>& points) const {
+
+    // The length of the track. This is calculated by adding the straight line distance between each point.
+    float length = Bezier::bezierLength(points);
+
+    //map from index to length of track at that index
+    std::unordered_map<int, float> lengthMap = Bezier::bezierLengthMap(points);
+    /*
+     * This will also initialize the accelerations along the track. At the beginning and end of the track, the acceleration is
+     * determined by the accel needed to get the pod up to speed. Between these points, the acceleration is the
+     * centripetal acceleration at the point. In the future, centripetal accel should be accounted for in the beginning
+     * and end of the route.
+     */
+
+    std::vector<float> accels;
+
+    float vfSquared = std::pow(DEFAULT_POD_MAX_SPEED, 2);
+    float v0Squared = 0.0;
+
+    //initialize velocity vector, the first value should be 0
+    std::vector<float> vels;
+    vels.push_back(0);
+
+    //used for getting the last index that the pod is ramping up at
+    float newVel1 = 0.0;
+
+    //Calculate velocities for ramping up, and fill the rest of the route with DEFAULT_POD_MAX_SPEED
+    for (int i = 1; i < points.size() - 1; i++) {
+        if (vels[i - 1] < DEFAULT_POD_MAX_SPEED) {
+            newVel1 = glm::sqrt(glm::pow(vels[i - 1], 2) + (2 * BEGIN_ACCEL * (lengthMap[i] - lengthMap[i - 1])));
+            vels.push_back(newVel1);
+        } else {
+            vels.push_back(DEFAULT_POD_MAX_SPEED);
+        }
+    }
+
+    //Get the index where you stop ramping up
+    int count = 0;
+    for (int i = 0; i < points.size(); i++) {
+        if (vels[i] < DEFAULT_POD_MAX_SPEED) {
+            count += 1;
+        } else {
+            count += 0;
+        }
+    }
+
+    int accel_upto_index = count - 1;
+
+    //used for getting the first index that the pod is winding down at
+    float newVel2 = 0.0;
+
+    //set all values after ramping up to 0 for velocity so that we can work backwards in next loop
+    for (int i = accel_upto_index + 1; i < points.size(); i++) {
+        vels[i] = 0.0;
+    }
+
+    //calculate where to start de-accelerating by working from the end of the route. Set all values in between
+    // to max accel
+    for (int i = points.size() - 2; i > accel_upto_index; i--) {
+        if (vels[i + 1] < DEFAULT_POD_MAX_SPEED) {
+            newVel2 = glm::sqrt(glm::pow(vels[i+1], 2) + (2 * END_ACCEL * (lengthMap[i+1] - lengthMap[i])));
+            vels[i] = newVel2;
+        } else {
+            vels[i] = DEFAULT_POD_MAX_SPEED;
+        }
+    }
+
+    //get the index where you start winding down
+    int count2 = 0;
+    for (int i = points.size() - 1; i > accel_upto_index; i--) {
+        if (vels[i] < DEFAULT_POD_MAX_SPEED) {
+            count2 += 1;
+        } else {
+            count += 0;
+        }
+    }
+
+
+    count2 = points.size() - count2;
+    int start_de_accel = count2;
+
+    //convert the indices for winding up and ramping down to a point in meters on the curve
+    float begin_accel_distance = lengthMap[accel_upto_index];
+    float end_accel_distance = lengthMap[start_de_accel];
+
+    //fill in the accelerations of the curve, use the centrip accel when in middle of curve
+    for (int i = 0; i < points.size() - 1; i++) {
+
+        if (lengthMap[i] <= begin_accel_distance) {
+            accels.push_back(BEGIN_ACCEL);
+        } else if (lengthMap[i] >= end_accel_distance) {
+            accels.push_back(END_ACCEL * -1);
+        } else if (lengthMap[i] > begin_accel_distance && lengthMap[i] < end_accel_distance) {
+            accels.push_back(calcCentripetalAccel(points[i - 1], points[i], points[i + 1]));
+        }
+    }
+
+
+    // Now we need to find all the points where the acceleration is larger than "g". For now we will ignore the
+    // beginning and the end of the route.
+
+    // this is a vector of the indices in the points and accels array where the acceleration is larger than g.
+    std::vector<int> accels_too_high;
+
+    // ignore first and last points
+    for (int i = accel_upto_index + 1; i < start_de_accel; i++) {
+        float centrip = calcCentripetalAccel(points[i - 1], points[i], points[i + 1]);
+        if (centrip > g) {
+            accels_too_high.push_back(i);
+        }
+    }
+
+    // Now we must go to all these points, using the velocity, and bringing it down to the velocity that it must be
+    // in order to make the centripetal acceleration at a good level.
+    for (int i = accel_upto_index + 1; i < accels_too_high.size(); i++) {
+        float radiusAtPoint = Bezier::radiusOfCurvature(points[accels_too_high[i] - 1],
+                                                        points[accels_too_high[i]],
+                                                        points[accels_too_high[i] + 1]);
+        float targetVel = glm::sqrt(g * radiusAtPoint);
+        float distanceToAccel = (_max_speed - targetVel) / (2 * DE_ACCEL);
+        float distanceFromStart = lengthMap[accels_too_high[i]];
+        float distanceToStartAccel = distanceFromStart - distanceToAccel;
+        float startIndex = 0.0;
+
+        //get index where slow down needs to start
+        for (int a = 0; a < accels_too_high[i]; a++) {
+            startIndex = lengthMap[a];
+            if (startIndex > distanceToStartAccel) {
+                a = accels_too_high[i];
+            }
+        }
+
+        // Correct the velocities. After this the accelerations and velocities are done.
+        for (int j = startIndex; j < accels_too_high[i]; j++) {
+            if (lengthMap[j] > (distanceFromStart - distanceToAccel)) {
+                vels[j] = glm::sqrt(glm::pow(vels[j-1], 2) + (2 * -1 * DE_ACCEL * (lengthMap[i] - lengthMap[i-1])));
+            }
+        }
+    }
+
+    //We now calculate the time for the pod to traverse the route
+    float time = 0.0;
+    float dist = 0.0;
+
+    //Calculate times.
+    for (int i = 1; i < vels.size() - 1 ; i++) {
+        dist = lengthMap[i+1] - lengthMap[i];
+        time += (dist / vels[i]);
+    }
+
+    return time;
+
+}
--- src/routes-tests/test_elevation.cpp
+++ src/routes-tests/test_elevation.cpp
@@ -1,1 +1,115 @@
-////  tests.cpp//  Routes////  Created by Logan on 11/10/17.//#define BOOST_TEST_MODULE Routes-Tests#include <boost/test/unit_test.hpp>#include <routes.h>BOOST_AUTO_TEST_CASE(test_elevation_conversions) {        // Load up a fake route to get some data    ElevationData data = ElevationData(glm::vec2(-118.9016667,34.9016667),                                       glm::vec2(-118.5000,34.08877925439021));        // Check some conversions    BOOST_CHECK(glm::dvec2(0.0, 0.0) == data.longitudeLatitudeToMeters(data.metersToLongitudeLatitude(glm::vec2(0.0, 0.0))));    BOOST_CHECK(glm::ivec2(0, 0)     == data.longitudeLatitudeToPixels(data.pixelsToLongitudeLatitude(glm::ivec2(0, 0))));        BOOST_CHECK(glm::ivec2(0, 0)     == data.metersToPixels(glm::dvec2(0.0, 0.0)));    BOOST_CHECK(glm::dvec2(0.0, 0.0) == data.convertPixelsToMeters(glm::ivec2(0, 0)));    }BOOST_AUTO_TEST_CASE(test_elevation_sampling) {        // Load up a fake route to get some data    ElevationData data = ElevationData(glm::vec2(-118.9016667,34.9016667),                                       glm::vec2(-118.5000,34.08877925439021));        // Sample using regular GDAL data    glm::vec3 sample_CPU = data.metersToMetersAndElevation(data.longitudeLatitudeToMeters(glm::dvec2(-118.701666666700030,34.901666666664143)));        // Generate the program    // Get stuff we need to execute a kernel on    const boost::compute::context& ctx = Kernel::getContext();    boost::compute::command_queue& queue = Kernel::getQueue();        static const std::string source = BOOST_COMPUTE_STRINGIZE_SOURCE(             __kernel void sample(__read_only image2d_t image, __global float* samples, float2 coord) {                           const sampler_t sampler = CLK_NORMALIZED_COORDS_TRUE | CLK_ADDRESS_CLAMP_TO_EDGE | CLK_FILTER_NEAREST;                          // Sample from given coord             samples[0] = read_imagef(image, sampler, coord).x;                      }        );        // Figure out where to sample from    glm::dvec2 sample_coord_g = glm::vec2(sample_CPU.x, sample_CPU.y) - data.getCroppedOriginMeters();    glm::dvec2 size = data.getCroppedSizeMeters();    boost::compute::float2_ sample_coord = boost::compute::float2_(sample_coord_g.x / size.x, sample_coord_g.y / size.y);        boost::compute::vector<float> buffer = boost::compute::vector<float>(1, ctx);    std::vector<float> buffer_CPU = std::vector<float>(1);    static Kernel kernel = Kernel(source, "sample");    kernel.setArgs(data.getOpenCLImage(), buffer.get_buffer(), sample_coord);    kernel.execute1D(0, 1);    boost::compute::copy(buffer.begin(), buffer.end(), buffer_CPU.begin(), queue);    BOOST_CHECK_CLOSE(buffer_CPU[0], sample_CPU.z, 5.0);    }BOOST_AUTO_TEST_CASE(test_elevation_samplingNE) {    // Load up a fake route to get some data    ElevationData data = ElevationData(glm::vec2(-71.099049,42.372044),                                       glm::vec2(-71.012393,42.315662));    // Sample using regular GDAL data    glm::vec3 sample_CPU = data.metersToMetersAndElevation(data.longitudeLatitudeToMeters(glm::dvec2(-71.05888,42.360082)));    // Generate the program    // Get stuff we need to execute a kernel on    const boost::compute::context& ctx = Kernel::getContext();    boost::compute::command_queue& queue = Kernel::getQueue();    static const std::string source = BOOST_COMPUTE_STRINGIZE_SOURCE(         __kernel void sample(__read_only image2d_t image, __global float* samples, float2 coord) {             const sampler_t sampler = CLK_NORMALIZED_COORDS_TRUE | CLK_ADDRESS_CLAMP_TO_EDGE | CLK_FILTER_NEAREST;             // Sample from given coord             samples[0] = read_imagef(image, sampler, coord).x;         }     );    // Figure out where to sample from    glm::dvec2 sample_coord_g = glm::vec2(sample_CPU.x, sample_CPU.y) - data.getCroppedOriginMeters();    glm::dvec2 size = data.getCroppedSizeMeters();    boost::compute::float2_ sample_coord = boost::compute::float2_(sample_coord_g.x / size.x, sample_coord_g.y / size.y);    boost::compute::vector<float> buffer = boost::compute::vector<float>(1, ctx);    std::vector<float> buffer_CPU = std::vector<float>(1);    static Kernel kernel = Kernel(source, "sample");    kernel.setArgs(data.getOpenCLImage(), buffer.get_buffer(), sample_coord);    kernel.execute1D(0, 1);    boost::compute::copy(buffer.begin(), buffer.end(), buffer_CPU.begin(), queue);    BOOST_CHECK_CLOSE(buffer_CPU[0], sample_CPU.z, 5.0);}
+//
+//  tests.cpp
+//  Routes
+//
+//  Created by Logan on 11/10/17.
+//
+
+#define BOOST_TEST_MODULE Routes-Tests
+#include <boost/test/unit_test.hpp>
+#include <routes.h>
+
+BOOST_AUTO_TEST_CASE(test_elevation_conversions) {
+    
+    // Load up a fake route to get some data
+    ElevationData data = ElevationData(glm::vec2(-118.9016667,34.9016667),
+                                       glm::vec2(-118.5000,34.08877925439021));
+    
+    // Check some conversions
+    BOOST_CHECK(glm::dvec2(0.0, 0.0) == data.longitudeLatitudeToMeters(data.metersToLongitudeLatitude(glm::vec2(0.0, 0.0))));
+    BOOST_CHECK(glm::ivec2(0, 0)     == data.longitudeLatitudeToPixels(data.pixelsToLongitudeLatitude(glm::ivec2(0, 0))));
+    
+    BOOST_CHECK(glm::ivec2(0, 0)     == data.metersToPixels(glm::dvec2(0.0, 0.0)));
+    BOOST_CHECK(glm::dvec2(0.0, 0.0) == data.convertPixelsToMeters(glm::ivec2(0, 0)));
+    
+}
+
+BOOST_AUTO_TEST_CASE(test_elevation_sampling) {
+    
+    // Load up a fake route to get some data
+    ElevationData data = ElevationData(glm::vec2(-118.9016667,34.9016667),
+                                       glm::vec2(-118.5000,34.08877925439021));
+    
+    // Sample using regular GDAL data
+    glm::vec3 sample_CPU = data.metersToMetersAndElevation(data.longitudeLatitudeToMeters(glm::dvec2(-118.701666666700030,34.901666666664143)));
+    
+    // Generate the program
+    // Get stuff we need to execute a kernel on
+    const boost::compute::context& ctx = Kernel::getContext();
+    boost::compute::command_queue& queue = Kernel::getQueue();
+    
+    static const std::string source = BOOST_COMPUTE_STRINGIZE_SOURCE(
+    
+         __kernel void sample(__read_only image2d_t image, __global float* samples, float2 coord) {
+             
+              const sampler_t sampler = CLK_NORMALIZED_COORDS_TRUE | CLK_ADDRESS_CLAMP_TO_EDGE | CLK_FILTER_NEAREST;
+             
+             // Sample from given coord
+             samples[0] = read_imagef(image, sampler, coord).x;
+             
+         }
+    
+    );
+    
+    // Figure out where to sample from
+    glm::dvec2 sample_coord_g = glm::vec2(sample_CPU.x, sample_CPU.y) - data.getCroppedOriginMeters();
+    glm::dvec2 size = data.getCroppedSizeMeters();
+    boost::compute::float2_ sample_coord = boost::compute::float2_(sample_coord_g.x / size.x, sample_coord_g.y / size.y);
+    
+    boost::compute::vector<float> buffer = boost::compute::vector<float>(1, ctx);
+    std::vector<float> buffer_CPU = std::vector<float>(1);
+
+    static Kernel kernel = Kernel(source, "sample");
+    kernel.setArgs(data.getOpenCLImage(), buffer.get_buffer(), sample_coord);
+    kernel.execute1D(0, 1);
+
+    boost::compute::copy(buffer.begin(), buffer.end(), buffer_CPU.begin(), queue);
+
+    BOOST_CHECK_CLOSE(buffer_CPU[0], sample_CPU.z, 5.0);
+    
+}
+
+BOOST_AUTO_TEST_CASE(test_elevation_samplingNE) {
+
+    // Load up a fake route to get some data
+    ElevationData data = ElevationData(glm::vec2(-71.099049,42.372044),
+                                       glm::vec2(-71.012393,42.315662));
+
+    // Sample using regular GDAL data
+    glm::vec3 sample_CPU = data.metersToMetersAndElevation(data.longitudeLatitudeToMeters(glm::dvec2(-71.05888,42.360082)));
+
+    // Generate the program
+    // Get stuff we need to execute a kernel on
+    const boost::compute::context& ctx = Kernel::getContext();
+    boost::compute::command_queue& queue = Kernel::getQueue();
+
+    static const std::string source = BOOST_COMPUTE_STRINGIZE_SOURCE(
+
+         __kernel void sample(__read_only image2d_t image, __global float* samples, float2 coord) {
+
+             const sampler_t sampler = CLK_NORMALIZED_COORDS_TRUE | CLK_ADDRESS_CLAMP_TO_EDGE | CLK_FILTER_NEAREST;
+
+             // Sample from given coord
+             samples[0] = read_imagef(image, sampler, coord).x;
+
+         }
+
+     );
+
+    // Figure out where to sample from
+    glm::dvec2 sample_coord_g = glm::vec2(sample_CPU.x, sample_CPU.y) - data.getCroppedOriginMeters();
+    glm::dvec2 size = data.getCroppedSizeMeters();
+    boost::compute::float2_ sample_coord = boost::compute::float2_(sample_coord_g.x / size.x, sample_coord_g.y / size.y);
+
+    boost::compute::vector<float> buffer = boost::compute::vector<float>(1, ctx);
+    std::vector<float> buffer_CPU = std::vector<float>(1);
+
+    static Kernel kernel = Kernel(source, "sample");
+    kernel.setArgs(data.getOpenCLImage(), buffer.get_buffer(), sample_coord);
+    kernel.execute1D(0, 1);
+
+    boost::compute::copy(buffer.begin(), buffer.end(), buffer_CPU.begin(), queue);
+
+    BOOST_CHECK_CLOSE(buffer_CPU[0], sample_CPU.z, 5.0);
+
+}
