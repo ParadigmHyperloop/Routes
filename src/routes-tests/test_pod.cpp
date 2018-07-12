e654e10d0ef89fe361d5808d71b66a3bd7599409
walkeri <walker.i@husky.neu.edu>
2018-07-12 18:27:30 -0400

    change TravisCI to use clang

--- src/routes-tests/test_pod.cpp
+++ src/routes-tests/test_pod.cpp
@@ -33,22 +33,22 @@ BOOST_AUTO_TEST_CASE(test_time_for_curve) {
     Pod pod1 = Pod(10);
 
     std::vector<glm::vec3> points1 = {{0, 0, 0},
-                                     {1, 1, 0},
-                                     {2, 2, 0}};
+                                      {1, 1, 0},
+                                      {2, 2, 0}};
 
     std::vector<glm::vec3> points2 = {{0, 0, 0},
-                                     {1, 1, 0},
-                                     {2, 2, 0},
-                                     {3, 3, 0},
-                                     {4, 4, 0},
-                                     {5, 5, 0},
-                                     {6, 6, 0},
-                                     {7, 7, 0},
-                                     {8, 8, 0},
-                                     {9, 9, 0},
-                                     {10, 10, 0},
-                                     {11, 11, 0},
-                                     {12, 12, 0}};
+                                      {1, 1, 0},
+                                      {2, 2, 0},
+                                      {3, 3, 0},
+                                      {4, 4, 0},
+                                      {5, 5, 0},
+                                      {6, 6, 0},
+                                      {7, 7, 0},
+                                      {8, 8, 0},
+                                      {9, 9, 0},
+                                      {10, 10, 0},
+                                      {11, 11, 0},
+                                      {12, 12, 0}};
 
     std::vector<glm::vec3> points3 = {{0, 3, 0},
                                       {1, 1, 0},
@@ -58,10 +58,6 @@ BOOST_AUTO_TEST_CASE(test_time_for_curve) {
                                       {5, 5, 0},
                                       {6, 7, 0},
                                       {8, 10, 0}};
-
-    std::cout << pod1.timeForCurve(points1) << std::endl;
-    std::cout << pod1.timeForCurve(points2) << std::endl;
-    std::cout << pod1.timeForCurve(points3) << std::endl;
 
     BOOST_CHECK_CLOSE(pod1.timeForCurve(points1), 0.0, epsilon);
     BOOST_CHECK_CLOSE(pod1.timeForCurve(points2), 1.34803f, epsilon);
